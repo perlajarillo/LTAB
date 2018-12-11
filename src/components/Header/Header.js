a80ca06d327fc61781500beadb064f056ab38b7c
perlajarillo <perlai.jarillo@gmail.com>
2018-12-11 13:12:50 -0500

    Removing unnecesary menu.

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -104,16 +104,6 @@ const NavAuthUser = props => {
                       tabIndex="-1"
                       color="inherit"
                       component={Link}
-                      to="/mentorsmessage"
-                    >
-                      Share message
-                    </Button>
-                  </List>
-                  <List>
-                    <Button
-                      tabIndex="-1"
-                      color="inherit"
-                      component={Link}
                       to="/"
                     >
                       Reports
