f8210f167372b6b9d62fa6395788b767fba675d2
perlajarillo <perlai.jarillo@gmail.com>
2019-01-04 11:40:01 -0500

    Including a firebase-admin app

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -18,4 +18,36 @@ const db = firebase.database();
 const auth = firebase.auth();
 const storage = firebase.storage();
 
-export { auth, db, storage };
+const admin = require("firebase-admin");
+//const serviceAccount = require("./serviceAccountKey.json");
+
+const refreshToken = require("./token.json"); // Get refresh token from OAuth2 flow
+
+const adminApp = admin.initializeApp(
+  {
+    credential: admin.credential.refreshToken(refreshToken),
+    databaseURL: process.env.REACT_APP_DATABASE_URL
+  },
+  "other"
+);
+/* const adminApp = admin.initializeApp(
+  {
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: "https://flad-mentorship.firebaseio.com"
+  },
+  "other"
+);
+ */
+/* const adminApp = admin.initializeApp(
+  {
+    credential: admin.credential.cert({
+      project_id: process.env.REACT_APP_PROJECT_ID,
+      client_email: process.env.REACT_APP_CLIENT_EMAIL,
+      private_key: process.env.REACT_APP_PRIVATE_KEY
+    }),
+    databaseURL: process.env.REACT_APP_DATABASE_URL
+  },
+  "other"
+);
+ */
+export { auth, db, storage, adminApp };
