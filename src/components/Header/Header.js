f2aa5a4a8bbdad27abb058b826b387db948635f6
perlajarillo <perlai.jarillo@gmail.com>
2019-01-22 17:03:39 -0500

    Including report component in the menu

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -137,18 +137,16 @@ const NavAuthUser = props => {
                 </div>
               ) : rol === "mentor" ? (
                 <div>
-                  {/*  we will removing the comment once the reports are ready
                   <List>
                     <Button
                       tabIndex="-1"
                       color="inherit"
                       component={Link}
-
-                      to="/"
+                      to="/mentorsreport"
                     >
                       Reports
                     </Button>
-                  </List> */}
+                  </List>
                 </div>
               ) : (
                 <List>
